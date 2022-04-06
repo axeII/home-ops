1031be1fb1d15e399acf6827d2dee8160fbac40a
axeII <axeII@users.noreply.github.com>
2022-04-06 20:50:34 +0200

    refactor(cloudflare): Upates main.tf

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -86,29 +86,29 @@ data "http" "ipv4" {
 ## Currently I don't want to open my site to web yet (even if it's behind cloudflare)
 
 
-# resource "cloudflare_record" "ipv4" {
-#   name    = "ipv4"
-#   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-#   value   = chomp(data.http.ipv4.body)
-#   proxied = true
-#   type    = "A"
-#   ttl     = 1
-# }
+resource "cloudflare_record" "ipv4" {
+  name    = "ipv4"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = chomp(data.http.ipv4.body)
+  proxied = true
+  type    = "A"
+  ttl     = 1
+}
 
-# resource "cloudflare_record" "root" {
-#   name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
-#   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-#   value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
-#   proxied = true
-#   type    = "CNAME"
-#   ttl     = 1
-# }
+resource "cloudflare_record" "root" {
+  name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
 
-# resource "cloudflare_record" "hajimari" {
-#   name    = "hajimari"
-#   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-#   value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
-#   proxied = true
-#   type    = "CNAME"
-#   ttl     = 1
-# }
+resource "cloudflare_record" "hajimari" {
+  name    = "hajimari"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
