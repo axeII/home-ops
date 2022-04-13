84c3965266f4632734946864e4175167c5790be0
axeII <axeII@users.noreply.github.com>
2022-04-13 23:04:16 +0200

    update(terraform): Adds more records to pihole

--- provision/terraform/pihole/main.tf
+++ provision/terraform/pihole/main.tf
@@ -65,3 +65,50 @@ resource "pihole_dns_record" "mylar_record" {
   domain = "mylar.${data.sops_file.pihole_secrets.data["domain"]}"
   ip     = "192.168.69.101"
 }
+
+resource "pihole_dns_record" "echo-server_record" {
+  domain = "echo-server.${data.sops_file.pihole_secrets.data["domain"]}"
+  ip     = "192.168.69.105"
+}
+
+resource "pihole_dns_record" "longhorn_record" {
+  domain = "longhorn.${data.sops_file.pihole_secrets.data["domain"]}"
+  ip     = "192.168.69.105"
+}
+
+resource "pihole_dns_record" "traefik_record" {
+  domain = "k-traefik.${data.sops_file.pihole_secrets.data["domain"]}"
+  ip     = "192.168.69.105"
+}
+
+### K8S ###
+
+resource "pihole_dns_record" "k8s-0_record" {
+  domain = "k8s-0"
+  ip     = "192.168.69.110"
+}
+
+resource "pihole_dns_record" "k8s-1_record" {
+  domain = "k8s-1"
+  ip     = "192.168.69.111"
+}
+
+resource "pihole_dns_record" "k8s-2_record" {
+  domain = "k8s-2"
+  ip     = "192.168.69.112"
+}
+
+resource "pihole_dns_record" "k8s-3_record" {
+  domain = "k8s-3"
+  ip     = "192.168.69.113"
+}
+
+resource "pihole_dns_record" "k8s-4_record" {
+  domain = "k8s-4"
+  ip     = "192.168.69.114"
+}
+
+resource "pihole_dns_record" "k8s-5_record" {
+  domain = "k8s-5"
+  ip     = "192.168.69.115"
+}
