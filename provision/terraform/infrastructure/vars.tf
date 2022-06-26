33a98bfbc422d0396d8759bb314c3926db4646b0
axeII <13370338+axeII@users.noreply.github.com>
2022-06-26 17:02:47 +0200

    refactor: Removes sixth node

--- provision/terraform/infrastructure/vars.tf
+++ provision/terraform/infrastructure/vars.tf
@@ -1,12 +1,11 @@
 variable "subnet_numbers" {
-  description = "Default resource group name that the network will be created in."
-  default = {
-      "k8s-0" = { ip = "110", node = "pve" }
-      "k8s-1" = { ip = "111", node = "node1" }
-      "k8s-2" = { ip = "112", node = "pve" }
-      "k8s-3" = { ip = "113", node = "node1" }
-      "k8s-4" = { ip = "114", node = "pve" }
-      "k8s-5" = { ip = "115", node = "pve" }
+description = "Default resource group name that the network will be created in."
+default = {
+    "k8s-0" = { ip = "110", node = "pve" }
+    "k8s-1" = { ip = "111", node = "node1" }
+    "k8s-2" = { ip = "112", node = "pve" }
+    "k8s-3" = { ip = "113", node = "node1" }
+    "k8s-4" = { ip = "114", node = "pve" }
     }
 }
 
