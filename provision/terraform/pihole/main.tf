bee0b918440db6802dd21818bec46015a51f0305
axeII <13370338+axeII@users.noreply.github.com>
2022-06-29 17:44:39 +0200

    refactor: Updates pihole

--- provision/terraform/pihole/main.tf
+++ provision/terraform/pihole/main.tf
@@ -38,21 +38,6 @@ resource "pihole_dns_record" "sabzbd_record" {
   ip     = "192.168.69.101"
 }
 
-resource "pihole_dns_record" "jellyfin_record" {
-  domain = "jellyfin.${data.sops_file.pihole_secrets.data["domain"]}"
-  ip     = "192.168.69.101"
-}
-
-resource "pihole_dns_record" "jellyseerr_record" {
-  domain = "jellyseerr.${data.sops_file.pihole_secrets.data["domain"]}"
-  ip     = "192.168.69.101"
-}
-
-resource "pihole_dns_record" "be_jellyfin_record" {
-  domain = "backend.jellyfin.${data.sops_file.pihole_secrets.data["domain"]}"
-  ip     = "192.168.69.101"
-}
-
 resource "pihole_dns_record" "proxmox_record" {
   domain = "proxmox.${data.sops_file.pihole_secrets.data["domain"]}"
   ip     = "192.168.69.99"
@@ -61,11 +46,6 @@ resource "pihole_dns_record" "proxmox_record" {
 resource "pihole_dns_record" "proxy_record" {
   domain = "proxy.${data.sops_file.pihole_secrets.data["domain"]}"
   ip     = "192.168.69.99"
-}
-
-resource "pihole_dns_record" "comic_record" {
-  domain = "comics.${data.sops_file.pihole_secrets.data["domain"]}"
-  ip     = "192.168.69.101"
 }
 
 resource "pihole_dns_record" "mylar_record" {
