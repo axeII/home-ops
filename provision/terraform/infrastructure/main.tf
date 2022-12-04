ecb188040c520bd23489892507fe41b316d499c2
axeII <13370338+axeII@users.noreply.github.com>
2022-12-04 01:16:55 +0100

    refactor: terraform infrastruture

--- provision/terraform/infrastructure/main.tf
+++ provision/terraform/infrastructure/main.tf
@@ -13,39 +13,45 @@ data "sops_file" "proxmox_secrets" {
 }
 
 provider "proxmox" {
-  pm_api_url          = "https://${var.proxmox_ip}:8006/api2/json"
-  pm_api_token_id     = data.sops_file.proxmox_secrets.data["pm_api_token_id"]
-  pm_api_token_secret = data.sops_file.proxmox_secrets.data["pm_api_token_secret"]
-  # leave tls_insecure set to true unless you have your proxmox SSL certificate situation fully sorted out (if you do, you will know)
-  pm_tls_insecure = true
+  pm_api_url  = "https://${var.proxmox_node1}:8006/api2/json"
+  pm_user     = data.sops_file.proxmox_secrets.data["pm_user"]
+  pm_password = data.sops_file.proxmox_secrets.data["pm_pass1"]
+  alias       = "node1"
 }
 
-resource "proxmox_vm_qemu" "k3s_server" {
-  # count = 4 # just want 1 for now, set to 0 and apply to destroy VM
+provider "proxmox" {
+  pm_api_url  = "https://${var.proxmox_node2}:8006/api2/json"
+  pm_user     = data.sops_file.proxmox_secrets.data["pm_user"]
+  pm_password = data.sops_file.proxmox_secrets.data["pm_pass2"]
+  alias       = "node2"
+}
 
-  for_each = var.subnet_numbers
-  name     = each.key
+provider "proxmox" {
+  pm_api_url  = "https://${var.proxmox_node3}:8006/api2/json"
+  pm_user     = data.sops_file.proxmox_secrets.data["pm_user"]
+  pm_password = data.sops_file.proxmox_secrets.data["pm_pass3"]
+  alias       = "node3"
+}
 
-  # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
-  target_node = each.value.node
+resource "proxmox_vm_qemu" "k8s-0" {
+  name        = "k8s-0"
+  provider    = proxmox.node1
+  target_node = "proxmox-node1"
+  clone       = var.template_name
 
-  # another variable with contents "ubuntu-2004-cloudinit-template"
-  clone = var.template_name
-
-  # basic VM settings here. agent refers to guest agent
   agent    = 1
   onboot   = "true"
   os_type  = "cloud-init"
-  cores    = 4
+  cores    = 8
   sockets  = 1
   cpu      = "host"
-  memory   = each.value.ram
+  memory   = "28672"
   scsihw   = "virtio-scsi-pci"
   bootdisk = "scsi0"
 
   disk {
     slot     = 0
-    size     = "50G"
+    size     = "180G"
     type     = "scsi"
     storage  = "local-lvm"
     iothread = 1
@@ -64,7 +70,97 @@ resource "proxmox_vm_qemu" "k3s_server" {
     ]
   }
 
-  ipconfig0 = "ip=192.168.69.${each.value.ip}/24,gw=192.168.69.1"
+  ipconfig0 = "ip=192.168.69.110/24,gw=192.168.69.1"
+
+  # sshkeys set using variables. the variable contains the text of the key.
+  sshkeys = <<EOF
+  ${data.sops_file.proxmox_secrets.data["ssh_key"]}
+  EOF
+}
+
+resource "proxmox_vm_qemu" "k8s-1" {
+  name        = "k8s-1"
+  provider    = proxmox.node2
+  target_node = "proxmox-node2"
+  clone       = var.template_name
+
+  agent    = 1
+  onboot   = "true"
+  os_type  = "cloud-init"
+  cores    = 8
+  sockets  = 1
+  cpu      = "host"
+  memory   = "28672"
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+
+  disk {
+    slot     = 0
+    size     = "180G"
+    type     = "scsi"
+    storage  = "local-lvm"
+    iothread = 1
+  }
+
+  # if you want two NICs, just copy this whole network section and duplicate it
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  # not sure exactly what this is for. presumably something about MAC addresses and ignore network changes during the life of the VM
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+
+  ipconfig0 = "ip=192.168.69.109/24,gw=192.168.69.1"
+
+  # sshkeys set using variables. the variable contains the text of the key.
+  sshkeys = <<EOF
+  ${data.sops_file.proxmox_secrets.data["ssh_key"]}
+  EOF
+}
+
+resource "proxmox_vm_qemu" "k8s-2" {
+  name        = "k8s-2"
+  provider    = proxmox.node3
+  target_node = "proxmox-node3"
+  clone       = var.template_name
+
+  agent    = 1
+  onboot   = "true"
+  os_type  = "cloud-init"
+  cores    = 8
+  sockets  = 1
+  cpu      = "host"
+  memory   = "28672"
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+
+  disk {
+    slot     = 0
+    size     = "500G"
+    type     = "scsi"
+    storage  = "local-lvm"
+    iothread = 1
+  }
+
+  # if you want two NICs, just copy this whole network section and duplicate it
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  # not sure exactly what this is for. presumably something about MAC addresses and ignore network changes during the life of the VM
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+
+  ipconfig0 = "ip=192.168.69.108/24,gw=192.168.69.1"
 
   # sshkeys set using variables. the variable contains the text of the key.
   sshkeys = <<EOF
--- provision/terraform/infrastructure/vars.tf
+++ provision/terraform/infrastructure/vars.tf
@@ -1,26 +1,19 @@
-variable "subnet_numbers" {
-  description = "Default resource group name that the network will be created in."
-  default = {
-    "k8s-0" = { ip = "110", node = "pve", ram = 10240 }
-    "k8s-1" = { ip = "111", node = "pve", ram = 10240 }
-    "k8s-2" = { ip = "112", node = "node1", ram = 9216 }
-    "k8s-3" = { ip = "113", node = "node1", ram = 9216 }
-    "k8s-4" = { ip = "114", node = "pve", ram = 10240 }
-  }
+variable "proxmox_node1" {
+  default = "proxmox-server01.juno.moe"
+  type    = string
 }
 
-variable "proxmox_ip" {
-  default = "192.168.69.100"
+variable "proxmox_node2" {
+  default = "proxmox-server02.juno.moe"
+  type    = string
 }
 
-variable "dns_server_ip" {
-  default = "192.168.69.101"
-}
-
-variable "proxmox_host" {
-  default = "pve"
+variable "proxmox_node3" {
+  default = "proxmox-server03.juno.moe"
+  type    = string
 }
 
 variable "template_name" {
-  default = "ubuntu-2004-cloudinit-template"
+  default = "fedora-cloudinit-template"
+  type    = string
 }
